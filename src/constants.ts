d243147aa02edef761c7b653585d1b9fdf32660d
Iñigo <ingonza85@gmail.com>
2022-12-26 13:45:58 +0100

    Updated identifiers

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const PLATFORM_NAME = 'ComelitVedo';
-export const PLUGIN_IDENTIFIER = 'homebridge-comelit-vedo-platform';
+export const PLATFORM_NAME = 'ComelitVedoCustomHaas85';
+export const PLUGIN_IDENTIFIER = 'homebridge-comelit-vedo-platform-custom-haas85';
